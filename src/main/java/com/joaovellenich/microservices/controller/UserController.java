8f19161867600875357848f64b3d8ba1f127590e
JoaoVellenich <jpvellenich@gmail.com>
2024-03-04 20:29:02 -0300

    feat: update user route

--- src/main/java/com/joaovellenich/microservices/controller/UserController.java
+++ src/main/java/com/joaovellenich/microservices/controller/UserController.java
@@ -2,7 +2,9 @@ package com.joaovellenich.microservices.controller;
 
 import com.joaovellenich.microservices.dto.user.CreateUserDTO;
 import com.joaovellenich.microservices.dto.user.GetUsersDTO;
+import com.joaovellenich.microservices.dto.user.UpdateUserDTO;
 import com.joaovellenich.microservices.useCases.user.*;
+import jakarta.websocket.server.PathParam;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +20,6 @@ import java.util.logging.Logger;
 public class UserController {
 
     private Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
-
     private final CreateUserUseCase createUserUseCase;
     private final GetUsersUseCase getUsersUseCase;
     private final GetUserByIdUseCase getUserByIdUseCase;
@@ -65,6 +66,23 @@ public class UserController {
             return ResponseEntity.ok().body(user);
         }catch (Exception error){
             logger.severe("Error - getUsersById - " + error.getMessage());
+            return ResponseEntity.badRequest().body(error.getMessage());
+        }
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable("id") UUID id, @RequestBody CreateUserDTO user){
+        try{
+            logger.info("Start - updateUser - " + id + " - " + user);
+            UpdateUserDTO updatedUser = UpdateUserDTO.builder()
+                    .id(id)
+                    .email(user.getEmail())
+                    .name(user.getName())
+                    .build();
+            this.updateUserUseCase.updateUser(updatedUser);
+            return ResponseEntity.ok().build();
+        }catch (Exception error){
+            logger.severe("Error - updateUser - " + error.getMessage());
             return ResponseEntity.badRequest().body(error.getMessage());
         }
     }
