8ce8f746bfdc2a4be92e2f81901cde08dda9dc12
JoaoVellenich <jpvellenich@gmail.com>
2024-03-04 13:53:38 -0300

    feat: creating dto

---
+++ src/main/java/com/joaovellenich/microservices/dto/user/GetUsersDTO.java
@@ -0,0 +1,18 @@
+package com.joaovellenich.microservices.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetUsersDTO {
+    private UUID id;
+    private String name;
+    private String email;
+}
