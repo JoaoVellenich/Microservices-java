680237d1161fc883062f710718b72860080ddba4
JoaoVellenich <jpvellenich@gmail.com>
2024-03-04 19:15:50 -0300

    feat: getting all users user route

--- src/main/java/com/joaovellenich/microservices/controller/UserController.java
+++ src/main/java/com/joaovellenich/microservices/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.joaovellenich.microservices.controller;
 
 import com.joaovellenich.microservices.dto.user.CreateUserDTO;
+import com.joaovellenich.microservices.dto.user.GetUsersDTO;
 import com.joaovellenich.microservices.useCases.user.*;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -45,5 +48,12 @@ public class UserController {
             logger.severe("Error - createUser route - " + error.getMessage());
             return ResponseEntity.badRequest().body(error.getMessage());
         }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<GetUsersDTO>> getAllUsers(){
+        logger.info("Start - getAllUsers");
+        var users = this.getUsersUseCase.getUsers();
+        return ResponseEntity.ok().body(users);
     }
 }
