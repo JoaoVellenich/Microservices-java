a0a265e8ae0b2154046639d4de863172284ad689
JoaoVellenich <jpvellenich@gmail.com>
2024-03-04 12:08:50 -0300

    Start project

---
+++ src/test/java/com/joaovellenich/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovellenich.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
